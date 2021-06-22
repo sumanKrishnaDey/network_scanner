ede0dd407b4673a8aac4304751915f10df8fc6c9
Suman <sumankrishnadey@gmail.com>
2021-06-22 12:38:22 -0400

    Snipper

--- network_scanner.py
+++ network_scanner.py
@@ -8,6 +8,7 @@ def scan(ip):
     broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
     arp_request_broadcast = broadcast / arp_request
     (answered_list) = scapy.srp(arp_request_broadcast, timeout = 1, verbose=False)[0]
+    arp_request_broadcast.show()
 
     client_list =[]
     for element in answered_list:
---
+++ packet_snipper.py
@@ -0,0 +1,12 @@
+#! usr/bin/env python3
+import scapy.all as scapy
+from scapy.layers import http
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        print(packet.show())
+
+sniff('eth0')
